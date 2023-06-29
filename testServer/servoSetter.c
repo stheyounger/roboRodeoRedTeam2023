2761f8c79bc91ff206e5ba27c16200c5c6829e11
stheyounger <sj2@penrose.us>
2023-06-29 08:22:54 -0700

    [testServer] gpio control via c

--- testServer/servoSetter.c
+++ testServer/servoSetter.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
 int main() {
-	gpio_set |= (1<<14);
+	printf("Hey there\n");
+	
+	if (wiringPiSetup () == -1)
+    		return 1 ;
+	
+	pinMode (0, OUTPUT) ;
+	
+	for (;;) {
+    		digitalWrite (0, 1) ;       // On
+    		delay (500) ;               // mS
+    		digitalWrite (0, 0) ;       // Off
+    		delay (500) ;
+  	}
+	
+	return 0;
 }
