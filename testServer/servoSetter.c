b6efca0750291b02310606e0659f172c39e8e3dc
stheyounger <sj2@penrose.us>
2023-06-29 09:21:15 -0700

    pwm signal working

--- testServer/servoSetter.c
+++ testServer/servoSetter.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include <wiringPi.h>
+#include <softPwm.h>
+
+void pinOnOffTest(int);
 
 int main() {
-	printf("Hey there\n");
+	printf("Pwm output test.\n");
 	
-	if (wiringPiSetup () == -1)
+	if (wiringPiSetup() == -1)
     		return 1 ;
 	
-	pinMode (0, OUTPUT) ;
-	
+	int targetPin = 23;
+
+//	pinOnOffTest(targetPin);
+
+	softPwmCreate(targetPin, 100, 100);
+	softPwmWrite(targetPin, 50);
+	int delayTime = 10;
 	for (;;) {
-    		digitalWrite (0, 1) ;       // On
-    		delay (500) ;               // mS
-    		digitalWrite (0, 0) ;       // Off
-    		delay (500) ;
+    		for (int bright = 0 ; bright < 100 ; ++bright) {
+      			softPwmWrite (1, bright) ;
+      			delay(delayTime) ;
+    		}
+		for (int bright = 100 ; bright >= 0 ; --bright) {
+      			softPwmWrite(1, bright) ;
+      			delay(delayTime);
+    		}
   	}
 	
 	return 0;
+}
+
+void pinOnOffTest(int targetPin) {
+        pinMode (targetPin, OUTPUT) ;
+        for (;;) {
+                digitalWrite (targetPin, 1) ;       // On
+                delay (500) ;               // mS
+                digitalWrite (targetPin, 0) ;       // Off
+                delay (500) ;
+        }       
+	
 }
