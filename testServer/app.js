8dd88cf871d662c064efc9356afdbbd1da02f77a
stheyounger <sj2@penrose.us>
2023-06-30 11:16:06 -0700

    got post working, send text from client

--- testServer/app.js
+++ testServer/app.js
@@ -6,6 +6,20 @@ const port = 8080
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+	const { headers, method, url } = req;
+	if (req.method == 'POST') {
+		console.log('data: ' + url.toString());
+//    		var body = ''
+//    		request.on('data', function(data) {
+//      			body += data
+//      			console.log('Partial body: ' + body)
+//    		})
+//    		request.on('end', function() {
+//      			console.log('Body: ' + body)
+//      			response.writeHead(200, {'Content-Type': 'text/html'})
+//      			response.end('post received')
+//    		})
+  	}
 	res.writeHead(200, {'Content-Type': 'text/html'})
   	fs.createReadStream('frontEnd/index.html').pipe(res)
 })
