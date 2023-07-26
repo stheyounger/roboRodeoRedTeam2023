5f2afd095d35c3ed216f09323e29623d54279b87
stheyounger <sj2@penrose.us>
2023-07-26 16:20:21 -0700

    [robotCode] sending data between pipes

--- robotCode/motorControlProgram.cpp
+++ robotCode/motorControlProgram.cpp
@@ -1,11 +1,129 @@
+#include "pca9685.h"
+#include <wiringPi.h>
+
+#include <string>
 #include <stdio.h>
-int main(int argc, char const *argv[]) {
-	printf("Running motor control program.\n");	
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+class ServoController {
+	public:
+		const int pinBase = 300;
+		const int maxPwm = 4096;
+		const int hertz = 50;
+	private: 
+		int calcTicks(float impulseMs) {
+			float cycleMs = 1000.0f / hertz;
+			return (int)(maxPwm * impulseMs / cycleMs + 0.5f);
+		}
+		float map(float input, float min, float max) {
+			return (input * max) + (1 - input) * min;
+		}
+	public:
+		int setupPwmDriver() {
+			wiringPiSetup();
+
+			// Setup with pinbase 300 and i2c location 0x40
+			int fd = pca9685Setup(pinBase, 0x40, hertz);
+			if (fd < 0) {
+				printf("Error in setup\n");
+				return fd;
+			}
+			
+			pca9685PWMReset(fd);
+
+			return 0;
+		}
+		void moveServo(int port, float milis) {
+			//printf("position in %f\n", milis);
+
+			float millis = map(milis, 1, 2);
+			//printf("after map %f\n", millis);
+		
+			int tick = calcTicks(millis);
+			//printf("after tick calc %d\n", tick);
+
+			pwmWrite(pinBase+port, tick);
+		}
+};
+
+
+
+class MotorCommand {
+	public:
+		int portNumber;
+		float position;
+		MotorCommand(int port, float pos) {
+			portNumber = port;
+			position = pos;
+		}
+};
+
+
+MotorCommand pipeDataToMotorCommand(char* data) {
+
+	string sData = data;
+	string dataWithoutKey = sData.substr(9, sData.length());
+	printf("dataWithoutKey %s\n", dataWithoutKey);
+	
+	int startOfSecondTerm = dataWithoutKey.find(" ");
+
+	string firstTerm = dataWithoutKey.substr(0, startOfSecondTerm);
+	string secondTerm = dataWithoutKey.substr(startOfSecondTerm, dataWithoutKey.length());
+	printf("firstTerm %s, secondTerm %s\n", firstTerm.c_str(), secondTerm.c_str());
+
+	int portNumber = stoi(firstTerm);
+	float position = stof(secondTerm);
+	cout << "portNumber " << portNumber << " position " << position << + "\n";	
+
+	MotorCommand motorCmd(portNumber, position);
+
+	cout << motorCmd.portNumber << " " << motorCmd.position << "\n";
+
+	return motorCmd;
+}
+
+bool checkForPipeData(char* data) {
+	string key = "pipeData";
+
+	string sData = data;
+	string potentialKey = sData.substr(0, key.length());
+	if(potentialKey.compare(key) == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+
+
+int main(int argc, char const* argv[]) {
+	printf("Running motor control program.\n");
+
+	ServoController servoCtrl;
+	servoCtrl.setupPwmDriver();
 
   	char* l = NULL;
   	size_t n;
 	while (getline(&l,&n, stdin) != -1 ) {
-      		printf("MCtrl, from stdin: %s",l);
+      		printf("MotorCtrl <- stdin: %s\n",l);
+
+		if (checkForPipeData(l)) {
+			printf("Pipe Data: %s\n", l);
+
+			MotorCommand motorCmd = pipeDataToMotorCommand(l);
+
+			printf("motorCmd: %i, %f\n", motorCmd.portNumber, motorCmd.position);
+
+			servoCtrl.moveServo(motorCmd.portNumber, motorCmd.position);
+		}	
+
     	}
+
+
 	return 0;
 }
--- robotCode/serverApp.js
+++ robotCode/serverApp.js
@@ -9,10 +9,11 @@ const server = http.createServer((req, res) => {
 	const { headers, method, url } = req;
 	if (req.method == 'POST') {
     		req.on('data', function(data) {
-    			const dataString = JSON.parse(data);
 			res.writeHead(200, {'Content-Type': 'text/plain'})
 			res.end("yup")
-			console.log(dataString)
+
+			console.log("Server recived: " + data);
+			handlePost(data)
 		})
   	}else{
 		res.writeHead(200, {'Content-Type': 'text/html'})
@@ -24,13 +25,41 @@ server.listen(port, hostname, () => {
  	console.log(`Server running at http://`+hostname+`:`+port+`/`)
 })
 
+function handlePost(data) {
+	if (isJson(data)) {
+		parsedData = JSON.parse(data);
 
-function sendData(dataJson) {
-	const dataString = JSON.parse(dataJson);
-	console.log(dataString);
-	fs.writeFile('dataFile.txt', dataString, err => {
-		if (err) {
-			console.log("err %s\n", err);
-		}
-	});
+		console.log("data.keyCode " + parsedData.keyCode);
+
+		sendDataToMotorController(convertKeyPressToAction(parsedData.keyCode))
+	}
+}
+
+function convertKeyPressToAction(keyPressed) {
+	switch (keyPressed) {
+		case "KeyA":
+			return { portNumber : "0", position : "1" };
+			break;
+		default:
+			return null;
+	}
+}
+
+function sendDataToMotorController(data) {
+	
+	if (data != null) {
+		const pipeData = "pipeData " + data.portNumber + " " + data.position
+		console.log(pipeData)
+	}
+}
+
+function isJson(item) {
+  let value = typeof item !== "string" ? JSON.stringify(item) : item;
+  try {
+    value = JSON.parse(value);
+  } catch (e) {
+    return false;
+  }
+
+  return typeof value === "object" && value !== null;
 }
